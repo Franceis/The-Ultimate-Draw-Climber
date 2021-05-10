39e26c53def8ca4be041d94ca3ade335fd5d15f4
Vinicius Franca <vinicusfranca10@gmail.com>
2021-05-10 17:47:45 -0300

    Finished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] private GameObject restartWindow;
+
+    private void Awake()
+    {
+        if(Instance == null) Instance = this; //Manage the GameManager instance for having one
+    }
+
+    private void Start()
+    {
+        restartWindow.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+    public void RestarWindowOn()
+    {
+        restartWindow.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
